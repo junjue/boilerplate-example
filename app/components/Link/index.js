ba8d0c3bc2c2d05a4e4abd912e97c83c6139d968
Junjue Wang <junwang@microsoft.com>
2019-01-08 15:49:22 -0500

    generate link component

---
+++ app/components/Link/index.js
@@ -0,0 +1,19 @@
+/**
+*
+* Link
+*
+*/
+
+import React from 'react';
+
+
+import styles from './styles.css';
+
+function Link() {
+  return (
+    <div className={styles.link}>
+    </div>
+  );
+}
+
+export default Link;
