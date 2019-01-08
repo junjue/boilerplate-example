9df2b10668b2dba40412de7997bb10a0535d0ebb
Junjue Wang <junwang@microsoft.com>
2019-01-08 13:29:02 -0500

    generate appbar

---
+++ app/components/AppBar/index.js
@@ -0,0 +1,19 @@
+/**
+*
+* AppBar
+*
+*/
+
+import React from 'react';
+
+
+import styles from './styles.css';
+
+function AppBar() {
+  return (
+    <div className={styles.appBar}>
+    </div>
+  );
+}
+
+export default AppBar;
