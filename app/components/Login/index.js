e632e599d7bdc63379d7dc48e2969753033f573e
Junjue Wang <junwang@microsoft.com>
2019-01-08 18:24:34 -0500

    login

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -7,24 +7,53 @@
 import React from 'react';
 
 import styles from './styles.css';
+import validator from 'email-validator';
+import classNames from 'classnames';
 
 class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes ={
+    login: React.PropTypes.func.isRequired,
+  }
+
+  state = {};
+
   login = () => {
-    console.log('login clicked', this.emailField.value);
+    const email = this.emailField.value;
+    if (!validator.validate(email)) {
+      this.setState({
+        errorText: 'Please provide a valid email',
+      });
+      return;
+    }
+
+    this.setState({
+      errorText: null,
+    });
+
+    this.props.login(email);
   }
 
   render() {
+    const fieldError = this.state.errorText ? (
+      <div className={styles.errorMessage}>
+        {this.state.errorText}
+      </div>
+    ) : null;
+
     return (
       <div className={styles.login}>
         <div className={styles.heading}>
           Login with your email
         </div>
         <input
-          className={styles.input}
+          className={classNames(styles.input, {
+            [styles.inputError]: this.state.errorText })}
           placeholder="your email"
           ref={(f) => { this.emailField = f; }}
           type="text"
         />
+
+        {fieldError}
 
         <div className={styles.actionContainer}>
           <div className={styles.button}>
