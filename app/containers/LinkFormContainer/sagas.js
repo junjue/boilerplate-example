b1a672010bc9620b661bd265b0dc27f8801eb17b
Junjue Wang <junwang@microsoft.com>
2019-01-09 13:51:27 -0500

    create link

--- app/containers/LinkFormContainer/sagas.js
+++ app/containers/LinkFormContainer/sagas.js
@@ -1,11 +1,23 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga';
+import { call } from 'redux-saga/effects';
+import { createLink } from '../../api';
+import { ADD_LINK } from './constants';
+
+function* addLink(action) {
+  try {
+    yield call(createLink, action.link);
+  } catch (e) {
+    console.log(e.message);
+  }
+}
 
 // Individual exports for testing
-export function* defaultSaga() {
-  return;
+export function* addLinkSaga() {
+  yield* takeLatest(ADD_LINK, addLink);
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  addLinkSaga,
 ];
